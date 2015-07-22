dce4fd59a874a9479316a0325add7c58ba569f41
craigricker <craigd.ricker@gmail.com>
2015-07-22 09:44:34 -0700

    Doing Homework 3
    Starting exercise one and two for assignment #3.  Get kat to check them
    over before submitting.

---
+++ C Class II/C2A3/C2-Assignment_3_Requirements/C2A3E2_TestDeclarations.cpp
@@ -0,0 +1,25 @@
+/*
+ *Craig Ricker, U06369876
+ *lucke.pirate@gmail.com
+ *_SP15_OL: C/C++ Programming II : Fundamental Programming Concepts, 109824, Ray Mitchell
+ *7/1/15
+ *C2A3E2_TestDeclarations.cpp
+ *Win7
+ *Visual C++ 11.0
+ *
+ *Explore type casting and declarations
+ *Email title: C2A3E2_U06369876
+ */
+
+void TestDeclarations()
+{
+   //One is below
+   void *vp = 0;
+   //Two is below
+   int fcnA(int val);
+   //Three is below
+   float **ppa[9];
+   //Four is below
+   float **&rppa[9] = ppa;
+   //Five is below
+}
