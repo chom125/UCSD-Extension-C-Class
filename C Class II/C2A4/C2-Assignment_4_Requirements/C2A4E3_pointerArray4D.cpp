964a52727c8463498c94fa11921daaa7e87ceac5
craigricker <craigd.ricker@gmail.com>
2015-08-04 12:23:08 -0700

    State DIagram
    Working on State Diagram

--- C Class II/C2A4/C2-Assignment_4_Requirements/C2A4E3_pointerArray4D.cpp
+++ C Class II/C2A4/C2-Assignment_4_Requirements/C2A4E3_pointerArray4D.cpp
@@ -1,26 +1,29 @@
-//
-//  C2A4E3_pointerArray4D.cpp
-//  C_Class
-//
-//  Created by Katherine Ellis on 7/29/15.
-//
-//
-
-#include <stdio.h>
-
+/*
+ *Craig Ricker, U06369876
+ *lucke.pirate@gmail.com
+ *_SP15_OL: C/C++ Programming II : Fundamental Programming Concepts, 109824, Ray Mitchell
+ *7/1/15
+ *C2A4E3_pointerArray4D.cpp
+ *Win7
+ *Visual C++ 11.0
+ *
+ *Creates a 4D pointer array from the methods in 13.26 and 13.17
+ *Email title: C2A4E3_U06369876
+ */
 extern const int DIM0 = 2, DIM1 = 3, DIM2 = 4, DIM3 = 5;
-float ixa[DIM3], ixb[DIM3], ixc[DIM3], ixd[DIM3];
-float iya[DIM3], iyb[DIM3], iyc[DIM3], iyd[DIM3];
-float iza[DIM3], izb[DIM3], izc[DIM3], izd[DIM3];
-float jxa[DIM3], jxb[DIM3], jxc[DIM3], jxd[DIM3];
-float jya[DIM3], jyb[DIM3], jyc[DIM3], jyd[DIM3];
-float jza[DIM3], jzb[DIM3], jzc[DIM3], jzd[DIM3];
-float *ix[DIM2] = {ixa, ixb, ixc, ixd};
-float *iy[DIM2] = {iya, iyb, iyc, iyd};
-float *iz[DIM2] = {iza, izb, izc, izd};
-float *jx[DIM2] = {jxa, jxb, jxc, jxd};
-float *jy[DIM2] = {jya, jyb, jyc, jyd};
-float *jz[DIM2] = {jza, jzb, jzc, jzd};
-float **i[DIM1] = {ix, iy, iz};
-float **j[DIM1] = {jx, jy, jz};
-float ***pointerArray4D[DIM0] = {i, j};
+//Made external so can be referenced elsewhere
+static float ixa[DIM3], ixb[DIM3], ixc[DIM3], ixd[DIM3];
+static float iya[DIM3], iyb[DIM3], iyc[DIM3], iyd[DIM3];
+static float iza[DIM3], izb[DIM3], izc[DIM3], izd[DIM3];
+static float jxa[DIM3], jxb[DIM3], jxc[DIM3], jxd[DIM3];
+static float jya[DIM3], jyb[DIM3], jyc[DIM3], jyd[DIM3];
+static float jza[DIM3], jzb[DIM3], jzc[DIM3], jzd[DIM3];
+static float *ix[DIM2] = {ixa, ixb, ixc, ixd};
+static float *iy[DIM2] = {iya, iyb, iyc, iyd};
+static float *iz[DIM2] = {iza, izb, izc, izd};
+static float *jx[DIM2] = {jxa, jxb, jxc, jxd};
+static float *jy[DIM2] = {jya, jyb, jyc, jyd};
+static float *jz[DIM2] = {jza, jzb, jzc, jzd};
+static float **iArray[DIM1] = {ix, iy, iz};
+static float **jArray[DIM1] = {jx, jy, jz};
+float ***pointerArray4D[DIM0] = {iArray, jArray};
--- C Class II/C2A5/C2-Assignment_5_Requirements/C2A5E1_SwapObjects.c
+++ C Class II/C2A5/C2-Assignment_5_Requirements/C2A5E1_SwapObjects.c
@@ -1,3 +1,15 @@
+/*
+ *Craig Ricker, U06369876
+ *lucke.pirate@gmail.com
+ *_SP15_OL: C/C++ Programming II : Fundamental Programming Concepts, 109824, Ray Mitchell
+ *8/4/15
+ *C2A5E1_SwapObjects.c
+ *Win7
+ *Visual C++ 11.0
+ *
+ *Through the use of pointers, swap two objects
+ *Email title: C2A5E1_U06369876
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,11 +19,11 @@ void SwapObjects(void *pa, void *pb, size_t size)
    //Need temporary storage to properly use memcpy
    void *tempStorage;
    //safely allocate memory for tempStorage of size size
-   if((tempStorage = malloc(size)) == NULL){
+   if ((tempStorage = malloc(size)) == NULL)
+   {
       fputs("Out of memory, sucks buddy\n", stderr);
       exit(EXIT_FAILURE);
    }
-   
    //uses memcpy to swap the items.  Need temp object
    memcpy(tempStorage, pb, size);
    memcpy(pb, pa, size);
