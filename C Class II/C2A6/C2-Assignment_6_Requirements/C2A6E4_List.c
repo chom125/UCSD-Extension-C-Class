101c2d90e742c4b3367796f5aa381bfe3cbfd072
craigricker <craigd.ricker@gmail.com>
2015-08-11 23:54:13 -0700

    Working on C2A6E4...gotta get going on it

---
+++ C Class II/C2A6/C2-Assignment_6_Requirements/C2A6E4_List.c
@@ -0,0 +1,18 @@
+/*
+ *Craig Ricker, U06369876
+ *lucke.pirate@gmail.com
+ *_SP15_OL: C/C++ Programming II : Fundamental Programming Concepts, 109824, Ray Mitchell
+ *8/10/15
+ *C2A6E4_List.c
+ *Win7
+ *Visual C++ 11.0
+ *
+ *Will create list, print, and then free it
+ *Email title: C2A6E4_U06369876
+ */
+#include "C2A6E4_List-Driver.h"
+
+List *CreateList(FILE *fp)
+{
+   list *next;
+}
---
+++ C Class II/C2A6/C2-Assignment_6_Requirements/C2A6E4_OpenFile.c
@@ -0,0 +1,26 @@
+/*
+ *Craig Ricker, U06369876
+ *lucke.pirate@gmail.com
+ *_SP15_OL: C/C++ Programming II : Fundamental Programming Concepts, 109824, Ray Mitchell
+ *8/10/15
+ *C2A6E4_OpenFile.c
+ *Win7
+ *Visual C++ 11.0
+ *
+ *Will safely open a file, and return a pointer to FILE
+ *Email title: C2A6E4_U06369876
+ */
+#include <stdio.h>
+#include <stdlib.h>
+FILE *OpenFile(const char *fileName)
+{
+   //Safely open a file and return a pointer to it
+   FILE *outFile;
+   if ((outFile = fopen(fileName, "r")) == NULL)
+       {
+          //If file cannot be opened, terminate program
+          fprintf(stderr, "File open failure\n");
+          exit(EXIT_FAILURE);
+       }
+   return outFile;
+}
