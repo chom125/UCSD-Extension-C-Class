ee2a2dc7a61944f67ddaa7ddeaf99eab288a109f
kkatellis <kkatellis@gmail.com>
2015-08-12 13:04:35 -0700

    kat

--- C Class II/C2A6/C2-Assignment_6_Requirements/C2A6E4_List.c
+++ C Class II/C2A6/C2-Assignment_6_Requirements/C2A6E4_List.c
@@ -12,6 +12,7 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "C2A6E4_List-Driver.h"
 #define MAX_STRING_SIZE 256
 List *NewList()
@@ -28,30 +29,31 @@ List *NewList()
 
 List *CreateList(FILE *fp)
 {
-   List *head, *ptr, *scanner;
+   List *head = NULL, *ptr, *scanner;
+    char *buffer;
    char rString[MAX_STRING_SIZE];
 
    //Need to loop through entire text document, looking for strings, ignore puncuation and spaces
-   for (head = NULL, rString = fscanf(fp, "%s", rString); 
-      rString != EOF; rString = fscanf(fp, "%s", rString))
+    while(fscanf(fp, "%s", rString) != EOF) {
       //Loop through current list looking if rString already present
-      for (scanner = head; scanner != NULL && scanner->str != rString; scanner = scanner->count)
-         ;
-      //If item found, add one to the count of that item
-      if (scanner != NULL)
-         scanner->count++;
-      else
-      {
-         //If no instance of string, will create a new list element
-         ptr = NewList();
-         //Sets the "next" value to the head of list (next element)
-         ptr->next = head;
-         //Saves ptr into head, so can reference next time around
-         head = ptr;
-         //Set count to zero
-         ptr->count = 0;
-         //Point str to the active string, rString...think this may need to be different
-         ptr->str = rString;
+        for (scanner = head; scanner != NULL && strcmp(scanner->str, rString); scanner = scanner->next);
+        //If item found, add one to the count of that item
+        if (scanner != NULL && strcmp(scanner->str, rString) == 0) {
+            scanner->count++;
+        } else {
+            //If no instance of string, will create a new list element
+            ptr = NewList();
+            //Sets the "next" value to the head of list (next element)
+            ptr->next = head;
+            //Set count to one
+            ptr->count = 1;
+            //Point str to the active string, rString...think this may need to be different
+            buffer = (char *)malloc(strlen(rString) + 1);
+            strcpy(buffer, rString);
+            ptr->str = buffer;
+            //Saves ptr into head, so can reference next time around
+            head = ptr;
+        }
       }
    return head;
 }
@@ -62,8 +64,9 @@ List *PrintList(const List *head)
    for (; head != NULL; head = head->next)
    {
       //Print the values...need to allign this properly
-      printf("%s%d ea\n", head->str, head->count);
+      printf("%s %d ea\n", head->str, head->count);
    }
+    return (List *)head;
 }
 
 void FreeList(List *head)
