cd8f575de3429ad77add7d739cbb0379abdbdc09
craigricker <craigd.ricker@gmail.com>
2015-08-17 17:25:30 -0700

    Working on C2A7E2 need to fix formatting but passing no errors

--- C Class II/C2A7/C2-Assignment_7_Requirements/C2A7E2_ListHex.cpp
+++ C Class II/C2A7/C2-Assignment_7_Requirements/C2A7E2_ListHex.cpp
@@ -1,16 +1,47 @@
+/*
+ *Craig Ricker, U06369876
+ *lucke.pirate@gmail.com
+ *_SP15_OL: C/C++ Programming II : Fundamental Programming Concepts, 109824, Ray Mitchell
+ *8/8/15
+ *C2A7E2_ListHex.cpp
+ *Win7
+ *Visual C++ 11.0
+ *
+ *This will read a file in binary mode, and print out hex representations of the data
+ *Email title: C2A7E2_U06369876
+ */
 #include <fstream>
 #include <iostream>
 #include <cstdlib>
 #include <iomanip>
 using namespace std;
 
-const int BITS_BYTE = 8;
+
+
+void *SafeMalloc(size_t size)
+{
+   //Safetly allocates memory
+   void *vp;
+   if ((vp = malloc(size)) == NULL)
+   {
+      //If not enough memory, exits
+      fputs("Out of memory\n", stderr);
+      exit(EXIT_FAILURE);
+   }
+   return(vp);
+}
 
 void ListHex(ifstream &inFile, int bytesPerLine)
 {
-   unsigned char *pointMe;
-   while (size--){
-      printf("%02x", )
+   //Set fill to 0
+   cout << setfill('0');
+   unsigned char curChar = inFile.get();
+   for (int byteCount = 1; inFile.good(); byteCount++) {
+      cout << hex << setw(2) << (int)curChar << ' ';
+      if (byteCount == bytesPerLine) {
+         cout << '\n';
+         byteCount = 0;
+      }
+      curChar = inFile.get();
    }
-
 }
